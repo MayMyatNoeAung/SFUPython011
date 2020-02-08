4ce7f9858d044ea4cdeb6c30a6a362f86c872afc
MayMyatNoeAung <maymyatnoealmas2001@gmail.com>
2020-02-08 12:19:12 +0800

    first commit

---
+++ tuple.py
@@ -0,0 +1,21 @@
+#Turple
+
+Turple = ()
+
+t = 12345, 54225, 'hello'
+t[0]
+
+t
+
+# Turple may be nested:
+u = t, (1, 2, 3, 4, 5)
+u
+
+ # Turple are imutable:
+ t[0] = 88888
+
+ # but they can contain suitable objects:
+ v = ([1, 2, 3], [3, 2, 1])
+ v
+
+ fruits = ("apple", "")
